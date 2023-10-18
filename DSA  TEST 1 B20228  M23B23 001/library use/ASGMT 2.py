458d2cb4820b9211aff4a6592e4775091f12145f
Joy7Ajay <133090131+Joy7Ajay@users.noreply.github.com>
2023-10-18 14:44:57 +0300

    adding comments

--- DSA  TEST 1 B20228  M23B23 001/library use/ASGMT 2.py
+++ DSA  TEST 1 B20228  M23B23 001/library use/ASGMT 2.py
@@ -1,13 +1,17 @@
-#2 Write a Python program to calculate the area of a parallelogram. Expected answer is 30.0
+#2 Write a Python program to calculate the area of a parallelogram. The expected answer is 30.
 import math
 
-area = []
+# Define the base and height of the parallelogram
+area = [] # make a list
 base = 5
 height = 6
 
+# Add the above to the list
 area.append(base)
 area.append(height)
 
 print("Length of base: ",base)
 print("Height of parallelogram: ",height)
+
+# This will multiply base to height
 print(math.prod(area))
